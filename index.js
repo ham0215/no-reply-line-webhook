104de8a518b26bda73f59e66e037345eb19c744e
naoto hamada <hamada@vis-its.com>
2020-03-30 22:52:43 +0900

    uninstall express

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-"use strict";
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+
+const app = express();
+
 
 exports.noReplyWebhook = (req, res) => {
   console.log('start webhook');
