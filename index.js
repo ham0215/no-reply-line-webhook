b58fed5aad65a7b16f1150b9e3ba326a348ff489
naoto hamada <hamada@vis-its.com>
2020-03-30 22:59:12 +0900

    sample echo bot

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const line = require('@line/bot-sdk');
-const express = require('express');
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -10,11 +9,27 @@ const config = {
 
 const client = new line.Client(config);
 
-const app = express();
-
-
 exports.noReplyWebhook = (req, res) => {
   console.log('start webhook');
-  res.send('Hello, World');
+
+  Promise.all(req.body.events.map(handleEvent))
+  .then((result) => res.json(result))
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  });
   console.log('end webhook');
 };
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
