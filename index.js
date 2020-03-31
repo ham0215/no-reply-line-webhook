14303ff90a95e4bf40a5896440d20ca4c4faaf94
naoto hamada <hamada@vis-its.com>
2020-03-31 22:53:16 +0900

    Fix noreply

--- index.js
+++ index.js
@@ -11,28 +11,28 @@ const config = {
 const client = new line.Client(config);
 
 exports.noReplyWebhook = (req, res) => {
-  console.log("start webhook");
-  console.log(req.body);
-
   const signature = crypto
     .createHmac("SHA256", config.channelSecret)
     .update(JSON.stringify(req.body))
     .digest("base64");
 
   if (signature != req.headers["x-line-signature"]) {
-    console.log("invalid signature");
     res.status(400).end();
   } else {
+    console.log(JSON.stringify(req.body));
+    res.status(200).end();
+    /*
     Promise.all(req.body.events.map(handleEvent))
       .then(result => res.json(result))
       .catch(err => {
         console.error(err);
         res.status(500).end();
       });
+      */
   }
-  console.log("end webhook");
 };
 
+/*
 function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     // ignore non-text-message event
@@ -45,3 +45,4 @@ function handleEvent(event) {
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
+*/
